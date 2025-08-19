2abd587be9a52a5cdfcc4c6145ba8dd7a5313d29
jameson-m-jolley <jameson.m.jolley@gmail.com>
2025-08-19 11:32:53 -0600

    update to some tests and types

--- src/root.zig
+++ src/root.zig
@@ -17,7 +17,14 @@ const testing = std.testing;
 //#7: We will leverage compiler-enabled safety features (e.g., debug and safe builds, sanitizers) during development and testing.
 //#8: Every function/macro that calls another function is required to have a unit test to validate its behavior and correctness.
 //NOTE: These tests must be declared in test blocks at the bottom function code they are testing.
-
+//#9 const's and vars that need to be computed then stored in a struct must fallow the naming convection and be padded with an _ so we can maintain the namespace
+// EXAMPLE:
+//blk:{
+// const _foo_ = ... ;
+// return struct {
+//          const foo = _foo_
+//      }
+//    }
 pub fn make_type_union(comptime byte_fields: anytype) type {
     _ = byte_fields;
     return union(enum) {
@@ -25,8 +32,8 @@ pub fn make_type_union(comptime byte_fields: anytype) type {
     };
 }
 test "make_type_union" {
-    std.debug.print("\n[test] make_type_union\n", .{});
-    std.debug.print("[test] calling make_type_union(type)", .{});
+    std.debug.print("\n[test] ___make_type_union___\n", .{});
+    std.debug.print("[test] calling make_type_union(type)\n", .{});
     const test_type = make_type_union(.{});
     std.debug.print("[test] creation of the type was successful {any}\n", .{test_type});
 }
@@ -45,26 +52,111 @@ pub fn make_protocol_type(comptime header_byte_fields: anytype, comptime message
     };
 }
 test "make_protocol_type" {
-    std.debug.print("\n[test] make_protocol_type\n", .{});
+    std.debug.print("\n[test] ___make_protocol_type___\n", .{});
     std.debug.print("[test] calling make_protocol_type(type)\n", .{});
     const test_type = make_protocol_type(.{}, .{});
     std.debug.print("[test] creation of type was successful {any}\n", .{test_type});
 }
 
-pub fn make_message_type(comptime byte_fields: anytype) type {
-    _ = byte_fields;
-    return struct {};
-}
-test "make_message_type" {}
-
 /// byte_type is the interpretation of the bytes if this is a nested field when the filed is inspected it will grab the parser for that field
-pub fn make_byte_field_type(comptime byte_type: type, comptime start_offset: u8) type {
-    _ = byte_type;
-    _ = start_offset;
-    return struct {
-        pub fn interpret_bytes(raw_bytes: []const u8) byte_type {}
+/// TODO make floats work
+/// TODO make non standard bit widths work
+/// as of now the float types and custom bit widths dont work and will return a 0 or a 0.0
+pub fn make_byte_field_type(comptime byte_type: type, comptime start_offset: u32, comptime endian: std.builtin.Endian, comptime msg_length: u32) type {
+    const info = @typeInfo(byte_type);
+    const _num_bits_ = info.int.bits;
+    const _num_bytes_: u32 = std.math.divCeil(u32, _num_bits_, 8) catch |err| switch (err) {
+        else => {
+            std.debug.print("Error: {any}\n", .{err});
+            return 0; // Return a fallback value
+        },
     };
+    switch (info) {
+        .int => {
+            const _bit_remainder_ = _num_bits_ % 8;
+            if (_bit_remainder_ != 0) {
+                return struct {
+                    const num_bits = _num_bits_;
+                    const num_bytes: u32 = _num_bytes_;
+                    const bit_remainder = _bit_remainder_;
+                    pub fn interpret_bytes(raw_bytes: [msg_length]u8) byte_type {
+                        _ = raw_bytes;
+                        return 0;
+                    }
+                };
+            }
+            return struct {
+                const num_bits = _num_bits_;
+                const num_bytes: u32 = _num_bytes_;
+                const bit_remainder = _bit_remainder_;
+                pub fn interpret_bytes(raw_bytes: [msg_length]u8) byte_type {
+                    const ret: byte_type = std.mem.readInt(byte_type, raw_bytes[start_offset .. start_offset + num_bytes], endian);
+                    return ret;
+                }
+            };
+        },
+        .float => {
+            //const bit_remainder = num_bits % 8;
+            return struct {
+                const num_bits = _num_bits_;
+                const num_bytes: u32 = _num_bytes_;
+                pub fn interpret_bytes(raw_bytes: [msg_length]u8) byte_type {
+                    _ = raw_bytes;
+                    _ = num_bytes;
+                    const ret: byte_type = 0.0;
+                    return ret;
+                }
+            };
+        },
+        else => {},
+    }
 }
 test "make_byte_field_type" {
-    std.debug.print("\n[test] make_byte_field_type\n", .{});
+    const rand = std.crypto.random;
+    const rand_bytes = blk: {
+        var temp_array: [64]u8 = undefined;
+        // This `inline for` loop runs at compile time
+        // and unrolls into three separate assignments.
+        inline for (0..64) |i| {
+            temp_array[i] = rand.int(u8);
+        }
+        break :blk temp_array;
+    };
+    std.debug.print("\n[test] ___make_byte_field_type___\n", .{});
+    std.debug.print("[test BF] obj creation BF Check\n", .{});
+    inline for (config.config().NUMBER_TYPES) |ty| {
+        const test_field = make_byte_field_type(ty, 0, .little, 64);
+        const res = test_field.interpret_bytes(rand_bytes);
+        std.debug.print("[test BF] interpreted bytes was [{any}]\n", .{res});
+        std.debug.print("[test BF] creation of type was successful {any}\n", .{test_field});
+    }
+    //_ = rand_bytes;
+    const raw_bytes: [8]u8 = .{ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08 };
+    std.debug.print("[test] testing Hex: 01 02 03 04 05 06 07 08\n", .{});
+    std.debug.print("[test 1] Reading a u32 (integer) with little endian\n", .{});
+    const field_u32_le = make_byte_field_type(u32, 0, .little, 8);
+    const value_u32_le = field_u32_le.interpret_bytes(raw_bytes);
+    try std.testing.expectEqual(@as(u32, 0x04030201), value_u32_le);
+    std.debug.print("[test 1] pass >> {any} = {any}\n", .{ @as(u32, 0x04030201), value_u32_le });
+}
+
+pub fn make_message_type(comptime byte_fields: anytype) type {
+    return struct {
+        const passed_struct_type = @TypeOf(byte_fields);
+        const name = @typeName(passed_struct_type); //_ = passed_struct_type;
+        const type_info = @typeInfo(passed_struct_type);
+
+        pub fn print() void {
+            std.debug.print("{str}\n", .{name});
+            inline for (type_info.@"struct".fields) |value| {
+                std.debug.print("{str} {any} {any} {any} {any}\n", value);
+            }
+        }
+    };
+}
+test "make_message_type" {
+    std.debug.print("\n[test] ___make_message_type___\n", .{});
+    const test_message = make_message_type(.{ .this = 5, .that = make_byte_field_type(u8, 0, .little, 64) });
+    test_message.print();
+    //std.debug.print("\n[test] make_message_type fields >> {any}\n", .{test_message.comptime_fields});
 }
